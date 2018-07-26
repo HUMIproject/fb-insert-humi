0c77186422a6685eb3dece3963d51ef2b1ebdae9
ProjectHUMI <35667081+ProjectHUMI@users.noreply.github.com>
2018-07-25 22:40:54 -0400

    Use MutationObserver to trigger adding HUMI node

--- fb3.js
+++ fb3.js
@@ -1,48 +1,29 @@
 
-document.body.style.border = "16px solid red";
-alert( "hello" );
+function mutCallback ( mutList ) {
 
-function mutCallback  ( mutList ) {
+    // Get the UFILikeLink elements that have NOT been marked with class "HUMI".
+    var elements   = document.body.querySelectorAll( ".UFILikeLink:not(.HUMI)" );
 
-    console.dir( mutList );
+    for ( var i = 0; i < elements.length; i++ ) {
 
-    // Handle each mutation in the mutation list.
-    for ( var i = 0; i < mutList.length; i++ ) {
+        // Get the element and its parent.
+	var elem   = elements[ i ];
+	var parent = elem.parentNode;
 
-        var mut = mutList[ i ];
-        console.log( mut );
+        // Create a new <span>HUMI</span> element.
+	var span   = document.createElement ( "span"   );
+	var text   = document.createTextNode( " HUMI " );
+	span.appendChild( text );
 
-        // Handle each of the added nodes for this mutation.
-        for ( var j = 0; j < mut.addedNodes.length; j++ ) {
-
-            // Look for class UFILikeLink in the subtree beneath this node.
-            var node     = mut.addedNodes[ j ];
-            var elements = node.getElementsByClassName( ".UFILikeLink" );
-
-            // Handle each UFILikeLink element.
-            for ( var k = 0; k < elements.length; k++ ) {
-
-                // Get the element and its parent.
-                var elem   = elements[ k ];
-                var parent = elem.parentNode;
-
-                // Create a new <span>HUMI</span> element.
-                var span   = document.createElement ( "span" );
-                var text   = document.createTextNode( "HUMI" );
-                span.appendChild( text );
-
-                // Insert the new element into the document.
-                parent.insertBefore( span, elem );
-                alert("mnhx");
-            }
-        }
+        // Insert the new span, and mark the element with class "HUMI".
+	parent.insertBefore( span, elem );
+	elem.classList.add( "HUMI" );
     }
 }
 
-// Create a new MutationObserver.
-var observer = new MutationObserver( mutCallback );
 // Observe mutations in the body subtree.
+var observer = new MutationObserver( mutCallback );
 observer.observe( document.body, { childList: true, subtree: true } );
 
-document.body.style.border = "16px solid blue";
+document.body.style.border = "16px solid red";
 
